fda585c3e46baf351697bc97029ff6d7dec1609f
spaciba <kshaug2@outlook.com>
2017-04-24 20:03:09 -0400

    enabled dir with no arguments, just gives current path

--- KBar/KBar.cpp
+++ KBar/KBar.cpp
@@ -10,6 +10,8 @@ int wmain()
 	String^ input = "";
 	int dir_ret;
 	String^ com_dir = "dir";
+	array<String^>^ tok_input;
+	int err;
 
 	do {
 		Console::WriteLine("Please enter a command");
@@ -17,24 +19,35 @@ int wmain()
 		
 		String^ delim = " ";
 		array<Char>^ delimin = delim->ToCharArray();
-		array<String^>^ tok_input;
+		
 		tok_input = input->Split(delimin);
 
 		
 
 		if (String::Equals(tok_input[0], "dir"))
 		{
-			int dirn = dir(tok_input[1]);
+			if (tok_input->Length < 2)
+			{
+				
+				TCHAR cdBuff[MAX_PATH];
+
+				GetCurrentDirectory(MAX_PATH, cdBuff);
+				
+
+				err = dir(gcnew System::String(cdBuff));
+			}
+			else
+				err = dir(tok_input[1]);
 		}
 
 		if (String::Equals(tok_input[0], "help"))
 		{
-			_tprintf(TEXT("Commands are: \n dir\ncd\nscreenshot\nget\nput\ntimestomp\nwinlist\nservlist\nproclist\n"));
+			_tprintf(TEXT("Commands are: \n dir -- dir [path to dir]\ncd\nscreenshot\nget\nput\ntimestomp\nwinlist\nservlist\nproclist\n"));
 		}
 
 
-		printf("Error: %d", dir_ret);
-	} while(!String::Compare(input, "exit"));
+		printf("Error: %d\n", dir_ret);
+	} while(!String::Equals(tok_input[0], "exit"));
 	
 }
 
